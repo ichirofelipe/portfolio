de5a79183f6842941f48f7ab68d7de6b22f1e4be
Ichiro Felipe <ichiro.felipe@playngo.com>
2023-08-13 21:07:12 +0800

    added responsive fix for smaller devices

--- assets/vue/components/Banner.vue
+++ assets/vue/components/Banner.vue
@@ -2,12 +2,12 @@
     <div class="banner flex items-center relative">
         
         <div class="banner-greeting flex flex-col">
-            <h1 class="text-8xl mb-20 font-bold">Nice to meet you!<br>
+            <h1 class="text-8xl mb-20 font-bold md:text-left text-center">Nice to meet you!<br>
                 I'm 
                 <span class="highlighted">Ichiro.</span>
             </h1>
 
-            <p>Based in the PH, I'm a skilled front-end developer with expertise in 
+            <p class="mb-5">Based in the PH, I'm a skilled front-end developer with expertise in 
                 translating PSD designs into pixel-perfect websites. With a strong command of HTML, 
                 CSS, and JavaScript, I excel in bringing visual concepts to life while ensuring a 
                 seamless user experience. Additionally, I have hands-on experience in developing 
